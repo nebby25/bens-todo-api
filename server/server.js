b08c1fc0ce0bfdda126333b3def9d11e049bde0d
nebby25 <nebby25@hotmail.com>
2017-07-07 11:10:57 -0600

    Add GET/ todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 // Local imports
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -9,6 +10,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// POST REQUEST
 app.post('/todos', (req, res)=>{
 //    console.log(req.body);
     var todo = new Todo({
@@ -20,13 +22,35 @@ app.post('/todos', (req, res)=>{
         res.status(400).send(err);
     });
 });
-
+// GET REQUEST
 app.get('/todos', (req, res)=>{
    Todo.find().then((todos)=>{
        res.send({todos});
    },(err)=>{
        res.status(400).send(e);
    }); 
+});
+
+app.get('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+       return res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+           return res.status(404).send();
+        } 
+       res.send(todo.text);
+        
+    }, (err)=>{
+       res.status(400).send();
+    });
+    // findById
+        // success
+            // if todo - send it back
+            // if no todo - 404
+        // err
+            // 400 - and send empty body back
 });
 
 app.listen(3000, ()=>{
