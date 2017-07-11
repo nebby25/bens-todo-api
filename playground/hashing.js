51c33a4d8b9ed821eedbe84cfc7252d6e71dc162
nebby25 <nebby25@hotmail.com>
2017-07-11 12:23:24 -0600

    Added tests for POST /users

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
+//bcrypt.genSalt(10, (err, salt)  =>{
+//    bcrypt.hash(password, salt, (err, hash) =>{
+//        console.log(hash);
+//    });
+//});
 
-var token = jwt.sign(data, 'boiseboyz');
-console.log(token);
-var decoded = jwt.verify(token, 'boiseboyz');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$owYmSIfaILFR7JC/vCzLqeINWmosd8M5Tqn3DDYx3vd/DGBEojXpq'
+
+bcrypt.compare(password, hashedPassword, (err, res)=>{
+    console.log(res);
+});
+//
+//var data = {
+//    id: 10
+//};
+//
+//
+//var token = jwt.sign(data, 'boiseboyz');
+//console.log(token);
+//var decoded = jwt.verify(token, 'boiseboyz');
+//console.log('decoded', decoded);
 
 //var message = 'I am user number 4';
 //var hash = SHA256(message).toString();
